e985198d6877c8a00b64ca532033003969593427
Aqib Ullah <aqibullah3312@gmail.com>
2025-12-15 12:33:26 +0500

    sanctum authentication done

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/login', [\App\Http\Controllers\AuthController::class,'login'])->name('login');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [\App\Http\Controllers\AuthController::class,'logout'])->name('logout');
+    Route::post('/profile', [\App\Http\Controllers\AuthController::class,'profile'])->name('profile');
+});
