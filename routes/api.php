e6faa60fd14375b72dc40a536c0e851ea32ec929
Aqib Ullah <aqibullah3312@gmail.com>
2025-12-16 13:52:18 +0500

    register router added

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('/login', [\App\Http\Controllers\AuthController::class,'login'])->name('login');
+Route::post('/register', [\App\Http\Controllers\AuthController::class,'register'])->name('register');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [\App\Http\Controllers\AuthController::class,'logout'])->name('logout');
