5fff077a89b084a0fda5fca431b065c88d671110
aqibullah <aqibullah3312@gmail.com>
2025-12-16 21:10:35 +0500

    layout routes handled + added custom check guard for routes

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -4,10 +4,34 @@ import axios from 'axios'
 
 import Login from '../pages/Login.vue'
 import Orders from '../pages/Orders.vue'
+import DashboardLayout from "../layouts/DashboardLayout.vue";
+import MainLayout from "../layouts/MainLayout.vue";
 
 const routes = [
-    { path: '/login', component: Login },
-    { path: '/', component: Orders }
+    {
+        path: '/login',
+        component: MainLayout,
+        meta: { requiresAuth: false },
+        children: [
+            {
+                path: '',
+                component: Login,
+            }
+        ]
+    },
+    {
+        path: '/',
+        component: DashboardLayout,
+        meta: { requiresAuth: true },
+        children:[
+            {
+                path:'',
+                component: Orders,
+                name: 'orders'
+            }
+        ]
+    },
+    { path: '/:pathMatch(.*)*', redirect: '/' }
 ]
 
 const router = createRouter({
@@ -15,23 +39,53 @@ const router = createRouter({
     routes
 })
 
+// Restore token on app start (important for page refresh)
+const savedToken = localStorage.getItem('auth_token')
+if (savedToken) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${savedToken}`
+}
+
 router.beforeEach(async (to, from, next) => {
     const userStore = useUserStore()
 
-    if (to.path === '/login') {
+    if (!to.meta.requiresAuth) {
+        if (!userStore.loaded) {
+            try {
+                await userStore.fetchProfile()
+            } catch (e) {
+                return next()
+            }
+        }
+
+        // if user already logged in then redirecting to home page
+        if (userStore.user) {
+            return next('/')
+        }
+
         return next()
     }
 
-    try {
+    // only for safe(protected) routes
+    if (to.meta.requiresAuth) {
         if (!userStore.loaded) {
-            await userStore.fetchProfile()
+            try {
+                await userStore.fetchProfile()
+            } catch (e) {
+                userStore.reset()
+                return next('/login')
+            }
         }
 
-        next()
-    } catch {
-        userStore.reset()
-        next('/login')
+        // user is authenticated
+        if (userStore.user) {
+            return next()
+        }
+
+        // unauthenticated : redirecting to login
+        return next('/login')
     }
+
+    next()
 })
 
 export default router
