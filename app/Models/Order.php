71c916550dc01ce951a6b22d6313f7979c975cd9
aqibullah <aqibullah3312@gmail.com>
2025-12-16 21:48:21 +0500

    added constants

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -11,6 +11,9 @@ class Order extends Model
 {
     use HasFactory;
 
+    const STATUS_OPEN = 1;
+    const STATUS_FILLED = 2;
+    const STATUS_CANCELLED = 3;
     protected $fillable = [
         'user_id',
         'symbol',
