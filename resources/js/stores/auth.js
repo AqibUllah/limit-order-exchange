a962a4c86ef91f3d783cc37b83fb868544693ea8
aqibullah <aqibullah3312@gmail.com>
2025-12-16 21:10:52 +0500

    logout core logic implemented

--- resources/js/stores/auth.js
+++ resources/js/stores/auth.js
@@ -1,23 +1,26 @@
 import {ref} from "vue";
 import {defineStore} from "pinia";
 import axios from 'axios'
+import {useRouter} from "vue-router";
 export const useAuthStore = defineStore('auth', () => {
 
     // defining store variables
     const loading = ref(false)
     const error = ref(null)
     const token = ref(null)
-
+    const router = useRouter()
     // implementations
     const login = async (credentials) => {
             loading.value = true
             error.value = null
             try {
                 await axios.get('/sanctum/csrf-cookie')
-                await axios.post('/api/login', credentials)
-                    .then(res => {
-                        token.value = res.data.token
-                    })
+                const res = await axios.post('/api/login', credentials)
+                token.value = res.data.token
+
+                // token handling
+                axios.defaults.headers.common['Authorization'] = `Bearer ${token.value}`
+                localStorage.setItem('auth_token', token.value)
             } catch (e) {
                 error.value = e.response?.data?.message || 'Login failed'
                 throw e
@@ -26,8 +29,20 @@ export const useAuthStore = defineStore('auth', () => {
             }
         }
 
-    const logout = async () => await axios.post('/api/logout')
+    const logout = async () => {
+        try{
+            await axios.post('/api/logout')
+        }catch (e){
 
+        } finally {
+            token.value = null
+            localStorage.removeItem('auth_token')
+            delete axios.defaults.headers.common['Authorization']
+            router.push('/')
+        }
+
+
+    }
 
     // returning all variables
     return {
