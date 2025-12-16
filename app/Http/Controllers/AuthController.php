f4fc98fd2c49424e49aca6e6fe75fed75361c7be
Aqib Ullah <aqibullah3312@gmail.com>
2025-12-16 13:50:21 +0500

    updated authentication functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,29 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
         $request->validate([
-            'email'    => 'required|email',
-            'password' => 'required'
+            'email' => 'required|email',
+            'password' => 'required',
         ]);
 
-        if (!Auth::attempt($request->only('email', 'password'))) {
+        if (! Auth::attempt($request->only('email', 'password'))) {
             return response()->json([
-                'msg' => 'Invalid credentials'
+                'msg' => 'Invalid credentials',
             ], 422);
         }
 
-        $request->session()->regenerate();
+        return $this->auth_response($request);
+    }
 
-        return response()->json([
-            'user' => Auth::user()
-        ]);
+    public function register(RegisterRequest $request)
+    {
+        try {
+
+            // registering new user
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => $request->password,
+
+            ]);
+
+            if ($user) {
+                if (! Auth::attempt($request->only('email', 'password'))) {
+                    return response()->json([
+                        'msg' => 'Invalid credentials',
+                    ], 422);
+                }
+            }
+
+            return $this->auth_response($request);
+
+        } catch (\Exception $exception) {
+            return response()->json([
+                'message' => $exception->getMessage(),
+            ], ResponseAlias::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
     }
 
     public function logout(Request $request)
@@ -35,18 +64,27 @@ class AuthController extends Controller
         $request->session()->regenerateToken();
 
         return response()->json([
-            'msg' => 'Logged out'
+            'msg' => 'Logged out',
         ]);
     }
 
     public function profile()
     {
         $user = Auth::user();
+
         return response()->json([
             'id' => $user->id,
             'email' => $user->email,
             'balance' => $user->balance,
-            'assets' => $user->assets
+            'assets' => $user->assets,
+        ]);
+    }
+
+    public function auth_response(Request $request)
+    {
+        return response()->json([
+            'user' => Auth::user(),
+            'token' => Auth::user()->createToken($request->email)->plainTextToken,
         ]);
     }
 }
