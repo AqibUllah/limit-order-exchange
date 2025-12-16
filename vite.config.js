794a5c29bf7679e305aac85fd060b69fd7a4882e
Aqib Ullah <aqibullah3312@gmail.com>
2025-12-16 13:51:31 +0500

    removed custom server configuration + vue plugin added

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
     plugins: [
+        vue(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss(),
     ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-        host: 'localhost', // instead of limit-order-exchange.test
-        port: 3000,        // or any free port you like
-        strictPort: true,  // optional: fail if port is taken
-    },
 });
